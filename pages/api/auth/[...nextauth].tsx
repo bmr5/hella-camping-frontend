bab1a07ecddbf018beeffab170cebbb6e9d4e104
ben ray <ben@bens-MacBook-Air.local>
2022-12-06 01:24:36 -0600

    fix auth types

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -3,11 +3,9 @@ import CognitoProvider from "next-auth/providers/cognito";
 
 export default NextAuth({
   providers: [
-    // AWS Cognito
     CognitoProvider({
-      clientId: process.env.COGNITO_CLIENT_ID,
-      clientSecret: process.env.COGNITO_CLIENT_SECRET,
-      //   domain: process.env.COGNITO_DOMAIN,
+      clientId: process.env.COGNITO_CLIENT_ID ?? "",
+      clientSecret: process.env.COGNITO_CLIENT_SECRET ?? "",
       issuer: process.env.COGNITO_ISSUER,
     }),
   ],
